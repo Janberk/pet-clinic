ee2b24aa799fe03d92941eb1d14890d12d2cce42
Canberk Demirkan <canberk.demirkan@gmail.com>
2020-12-20 01:37:51 +0100

    Add map services for mocking persistence

---
+++ pet-clinic-data/src/main/java/de/canberk/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package de.canberk.petclinic.services.map;
+
+import de.canberk.petclinic.model.Owner;
+import de.canberk.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+}
---
+++ pet-clinic-data/src/main/java/de/canberk/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package de.canberk.petclinic.services.map;
+
+import de.canberk.petclinic.model.Vet;
+import de.canberk.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+}
